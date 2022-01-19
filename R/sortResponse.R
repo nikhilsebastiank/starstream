bd66af68f2da808fffe951259c7ea0097cf8ad6e
Nikhil Sebastian <80044373+nikhilsebastiank@users.noreply.github.com>
2022-01-19 10:20:09 +0100

    removed argument in sortresponse

--- R/sortResponse.R
+++ R/sortResponse.R
@@ -7,10 +7,34 @@ usethis::use_package("utils")
 #' @export
 #' @importFrom stats na.omit
 #' @importFrom utils View
-sortresponse = function(a){
+sortresponse = function(){
+  rawData = readxl::read_xlsx("/Users/nikhilsebastian/Dropbox/Projects/MasterThesis/data/UK_Creators__Earnings_Survey_Raw_Data.xlsx")
+  colnames(rawData) = rawData[1,] # Changes the column names based on the answer number
+  rawData = rawData[-1,] # Removes the first row (corresponds to column names)
+  rawData[is.na(rawData)] = "0" # missing values coded as 0s
+
+  # Vectors of column names
+  namesA = colnames(rawData[1:40])
+  namesA = append(namesA, colnames(rawData[194:222]))
+  namesA = sort(namesA)
+  namesB = colnames(rawData[41:95])
+  namesB = sort(namesB)
+  namesC = colnames(rawData[96:135])
+  namesC = sort(namesC)
+  namesD = colnames(rawData[136:159])
+  namesD = sort(namesD)
+  namesE = colnames(rawData[160:193])
+  namesE = sort(namesE)
+  namesF = colnames(rawData[223:228])
+  namesF = sort(namesF)
+  namesQ = namesB[53:55]
+  namesB = namesB[-(53:55)]
+  namesQ = sort(namesQ)
+  rawData2 = rawData[,c(namesA,namesB,namesC,namesD,namesE,namesF,namesQ)]
+
   undervector = c()
   lettervector = c()
-  for (i in colnames(a)){
+  for (i in colnames(rawData2)){
     d = substring(i,1,3)
     if (substring(d,3,3) == "_"){
       undervector = append(undervector,d)
@@ -20,18 +44,21 @@ sortresponse = function(a){
     }
   }
   for (i in lettervector){
-    c = stringr::str_extract(colnames(a), stringr::str_c("(",i,").","*"))
+    c = stringr::str_extract(colnames(rawData2), stringr::str_c("(",i,").","*"))
     c = as.character(na.omit(c))
     if (length(c) > 1){
-      a = mergecol(a,i)
+      rawData2 = mergecol(rawData2,i)
     }
   }
   for (i in undervector){
-    c = stringr::str_extract(colnames(a), stringr::str_c("(",i,").","*"))
+    c = stringr::str_extract(colnames(rawData2), stringr::str_c("(",i,").","*"))
     c = as.character(na.omit(c))
     if (length(c) > 1){
-      a = mergecol(a,i)
+      rawData2 = mergecol(rawData2,i)
     }
   }
-  a
+  rawData2
+  saveRDS(rawData2, file =  "/Users/nikhilsebastian/Dropbox/Projects/MasterThesis/results/data/sortedCreators.Rds")
+  sortedCreators = readRDS( file = "/Users/nikhilsebastian/Dropbox/Projects/MasterThesis/results/data/sortedCreators.Rds")
+  return(sortedCreators)
 }
