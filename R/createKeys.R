27284679f1dfe7c29a59cbdb6123e64e12769513
Nikhil  Sebastian <nikhilsebastian@HUAWEI_Y9s-8f5c0647e2ed90.localdomain>
2022-02-23 21:40:25 +0100

    added import readxl

--- R/createKeys.R
+++ R/createKeys.R
@@ -1,12 +1,13 @@
 usethis::use_package("readxl")
 
 #' Creates Unique Keys from Music Brainz Database
+#' @import readxl
 #' @importFrom utils write.csv
 #' @return A .csv file containing the artist keys and artist names
 #' @export
 #'
 createKeys = function(){
-  artistdata = read_xlsx("data/artistkeys.xlsx")
+  artistdata = readxl::read_xlsx("data/artistkeys.xlsx")
   View(artistdata)
   write.csv(artistdata, "data/artistkeysmain.csv", row.names = FALSE)
 
