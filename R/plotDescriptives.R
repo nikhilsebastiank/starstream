45629a395a0aa7cc8e4914b6686739db9158434c
Nikhil  Sebastian <nikhilsebastian@Nikhils-MacBook-Pro.local>
2022-04-26 12:13:27 +0200

    reduced number of density plots for clarity

--- R/plotDescriptives.R
+++ R/plotDescriptives.R
@@ -22,14 +22,14 @@ plotDescriptives = function(workingdir = "~/Dropbox/Projects/MasterThesis", resu
   evolution <- ggplot2::ggplot(data = charts)+
     ggplot2::geom_density(data = dplyr::filter(charts, year == '2020'), ggplot2::aes(x = indicativerevenue, color = '2020'))+
     ggplot2::geom_density(data = dplyr::filter(charts, year == '2019'), ggplot2::aes(x = indicativerevenue, color = '2019'))+
-    ggplot2::geom_density(data = dplyr::filter(charts, year == '2018'), ggplot2::aes(x = indicativerevenue, color = '2018'))+
-    ggplot2::geom_density(data = dplyr::filter(charts, year == '2017'), ggplot2::aes(x = indicativerevenue, color = '2017'))+
+    #ggplot2::geom_density(data = dplyr::filter(charts, year == '2018'), ggplot2::aes(x = indicativerevenue, color = '2018'))+
+    #ggplot2::geom_density(data = dplyr::filter(charts, year == '2017'), ggplot2::aes(x = indicativerevenue, color = '2017'))+
     ggplot2::geom_density(data = dplyr::filter(charts, year == '2016'), ggplot2::aes(x = indicativerevenue, color = '2016'))+
-    ggplot2::geom_density(data = dplyr::filter(charts, year == '2015'), ggplot2::aes(x = indicativerevenue, color = '2015'))+
-    ggplot2::geom_density(data = dplyr::filter(charts, year == '2014'), ggplot2::aes(x = indicativerevenue, color = '2014'))+
+    #ggplot2::geom_density(data = dplyr::filter(charts, year == '2015'), ggplot2::aes(x = indicativerevenue, color = '2015'))+
+    #ggplot2::geom_density(data = dplyr::filter(charts, year == '2014'), ggplot2::aes(x = indicativerevenue, color = '2014'))+
     ggplot2::geom_density(data = dplyr::filter(charts, year == '2013'), ggplot2::aes(x = indicativerevenue, color = '2013'))+
-    ggplot2::geom_density(data = dplyr::filter(charts, year == '2012'), ggplot2::aes(x = indicativerevenue, color = '2012'))+
-    ggplot2::geom_density(data = dplyr::filter(charts, year == '2011'), ggplot2::aes(x = indicativerevenue, color = '2011'))+
+    #ggplot2::geom_density(data = dplyr::filter(charts, year == '2012'), ggplot2::aes(x = indicativerevenue, color = '2012'))+
+    #ggplot2::geom_density(data = dplyr::filter(charts, year == '2011'), ggplot2::aes(x = indicativerevenue, color = '2011'))+
     ggplot2::geom_density(data = dplyr::filter(charts, year == '2010'), ggplot2::aes(x = indicativerevenue, color = '2010'))+
     ggplot2::labs(title = 'Evolution of Income Distribution for Superstars', x = 'Revenue', y = 'Density')
   evolution
